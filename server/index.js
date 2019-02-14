64289a4c3f099b887eed58ed71d691269bb73952
Felipe <phil.sousa@gmail.com>
2019-02-14 12:36:59 -0800

    creating endpoints in my server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var rp = require('request-promise');
+
 var parser = require('body-parser');
 
 var request = require('request');
@@ -18,9 +20,18 @@ app.listen(PORT, (err) => {
   }
 });
 
-app.get('/videos/:video_id', (req, res) => {
-
+app.post('/videos/:video_id', (req, res) => {
+  
   var movie = req.body;
   var category = req.body.Categories[0];
+  console.log(category);
+  request.get('https://6eac8a25-e35d-4ef9-857f-b4c542a041b5.mock.pstmn.io/demo', (err, results) => {
+    if (err) {
+      console.log('there was an error');
+    } else {
+      console.log('results are here', results.body);
+      res.send(results.body);
+    }
+  });
   
 });
