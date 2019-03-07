a3a65554ac1a7bccd46072cc9853d6492dbb7241
Felipe <phil.sousa@gmail.com>
2019-03-06 17:23:47 -0800

    creating database with mlab

--- client/source/components/App.jsx
+++ client/source/components/App.jsx
@@ -38,7 +38,7 @@ export default class App extends Component {
        .then((thumbnailsFromOther) => {
         console.log('here are the thumbnails=============', thumbnailsFromOther);
         let myUrl = `http://sidebar-component-env.phjkgcp7vm.us-east-2.elasticbeanstalk.com`
-        Axios.get(`${myUrl}/featured/${targetCategory}`)
+        Axios.get(`${url}/${targetCategory}`)
 
         .then((thumbnailsFromDb) => {
           console.log('these are the movies from database', thumbnailsFromDb);
--- database/index.js
+++ database/index.js
@@ -1,5 +1,14 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/movies', { useNewUrlParser: true });
+
+mongoose.connect('mongodb+srv://philxavier:secreta1@cluster0-9iu0m.mongodb.net/fec-database?retryWrites=true', { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log('there was an error', err)
+      
+  } else {
+    console.log('connected')
+  }
+    
+});
 
 const movieSchema = new mongoose.Schema({
   url: String,
@@ -16,7 +25,6 @@ const Movie = mongoose.model('Movie', movieSchema);
 let findMoviesByCategory = (inputCategory) => {
   inputCategory = inputCategory.toLowerCase();
   var query = {category:inputCategory};
-
   return new Promise((resolve, reject) => {
     Movie.find(query, (err, res) => {
       if (err) {
