5688a44a8cf73f0c952f693e62dad770f5a22749
Felipe <phil.sousa@gmail.com>
2019-03-07 19:02:55 -0800

    changes in video.jsx and video container

--- client/source/components/Video.jsx
+++ client/source/components/Video.jsx
@@ -7,17 +7,17 @@ export default class Video extends Component {
   }
 
   render() {
-    let {thumbnails, changeVideo} = this.props
+    let {thumbnails, changeVideo} = this.props;
     var randomId = Math.floor(Math.random() * 100); 
     return (
       <div>
         {thumbnails.map((ele, ind) => {
           return <div className="thumbnailContainer">
-          <div className="imageContainer" onClick = {() => {changeVideo(randomId)}} >
-            {ind === 0 ? <img className="firstItem thumbnailsImage" key = {ind} src={ele.thumbnail}/> : 
-              <img key = {ind} className="thumbnailsImage" src={ele.thumbnail}/>
-            }
-          </div>
+            <div className="imageContainer" onClick = {() => { changeVideo(randomId) ;}} >
+              {ind === 0 ? <img className="firstItem thumbnailsImage" key = {ind} src={ele.thumbnail}/> : 
+                <img key = {ind} className="thumbnailsImage" src={ele.thumbnail}/>
+              }
+            </div>
             <div className="videoTextContainer">
               <div className="videoName">{ele.title}</div>
               <div className="videoAuthor">{ele.author}</div>
--- client/source/components/VideoContainer.jsx
+++ client/source/components/VideoContainer.jsx
@@ -3,7 +3,7 @@ import Video from './Video.jsx';
 
 export default class VideoContainer extends Component {
   render() {
-    let {thumbnails, changeVideo} = this.props
+    let {thumbnails, changeVideo} = this.props;
     return (
       <div id="videoContainer">
         <Video thumbnails = {thumbnails} changeVideo = {changeVideo} />
