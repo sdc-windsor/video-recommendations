73bcf832de7314dbc2757d5e8b479994e1627339
Chen Wilson <chenlwilson@gmail.com>
2019-04-01 22:51:32 -0700

    Added default videos when ajax fails

--- utils/getVideos.js
+++ utils/getVideos.js
@@ -1,5 +1,18 @@
 const $ = require('jquery');
 
+const defaultVideo = {
+  author: 'Hack Reactor',
+  title: 'How the Internet Works',
+  thumbnail: 'https://s3-us-west-1.amazonaws.com/elasticbeanstalk-us-west-1-730513610105/images/1.jpg',
+};
+
+const defaultVideos = [];
+for (let i = 0; i < 10; i + 1) {
+  defaultVideos.push(defaultVideo);
+}
+
+console.log(defaultVideos);
+
 const getVideos = (id, callback) => {
   $.ajax({
     url: `http://localhost:3002/recommendations/${id}`,
@@ -35,13 +48,18 @@ const getVideosGQL = (id, callback) => {
     }),
   })
     .done((response) => {
-      console.log(response.data.getRecommendations);
-      callback(response.data.getRecommendations);
+      const videos = response.data.getRecommendations;
+      console.log(videos);
+      if (videos.length < 10) {
+        callback(videos.concat(defaultVideos.slice(0, 10 - videos.length)));
+      } else {
+        callback(response.data.getRecommendations);
+      }
     })
     .fail(() => {
       console.log('fail to get data from graphql');
-      // send back default data
-      // callback(defaultVideos)
+      // send back 10 default videos
+      callback(defaultVideos);
     });
 };
 
